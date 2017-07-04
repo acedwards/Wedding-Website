3f384891a06c848e27c68491f9551bfb4fc4f6da
Andrea Stobo <andrea.stobo@gmail.com>
2017-07-03 20:37:52 -0400

    basic database and auth setup

--- public/js/form-control.js
+++ public/js/form-control.js
@@ -1,20 +1,29 @@
 function validate(firstName, lastName) {
-	var fullName = firstName + ' ' + lastName;
-	
+	var fullName = firstName.val() + ' ' + lastName.val();
+	var database = firebase.database().ref('/guests/' + fullName).once('value').then(function(snapshot){
+		console.log(snapshot.val().isComing);
+	});
 }
 
-$('#rsvpForm').on('submit', function(e) {
-	var firstName = $('#firstName');
-	var lastName = $('#lastName');
+$('#next').on('click', function(e) {
+	firebase.auth().signInAnonymously().catch(function(error) {
+	// Handle Errors here.
+	var errorCode = error.code;
+	var errorMessage = error.message;
+	// ...
+	}).then(function() {
+		var firstName = $('#firstName');
+		var lastName = $('#lastName');
 
-	var isValid = validate(firstName, lastName);
+		var isValid = validate(firstName, lastName);
+	});
 
-	if(isValid){
+	// if(isValid){
 
 
-	} else {
-		//show alert?
-		e.preventDefault();
-	}
+	// } else {
+	// 	//show alert?
+	// 	e.preventDefault();
+	// }
 
 });
