841953013cffb69a213448c073b084da372ea85d
Andrea Stobo <andrea.stobo@gmail.com>
2017-07-04 22:46:56 -0400

    show error message when not on guest list

--- public/js/form-control.js
+++ public/js/form-control.js
@@ -1,7 +1,15 @@
 function validate(firstName, lastName) {
 	var fullName = firstName.val() + ' ' + lastName.val();
 	var database = firebase.database().ref('/guests/' + fullName).once('value').then(function(snapshot){
-		console.log(snapshot.val().isComing);
+		//if null return false else return true console.log(snapshot.val().isComing);
+		if(snapshot.val() === null){
+			console.log('snapshot is null');
+			return false;
+			
+		}else {
+			console.log('snapshot has stuff');
+			return snapshot.val();
+		}
 	});
 }
 
@@ -14,16 +22,23 @@ $('#next').on('click', function(e) {
 	}).then(function() {
 		var firstName = $('#firstName');
 		var lastName = $('#lastName');
-
 		var isValid = validate(firstName, lastName);
+
+		if(isValid === false){
+			$('.form-group').addClass('has-error');
+			var feedbackDiv = $('#rsvp-error-message-1');
+			feedbackDiv.html("Oops! That name is not on the guest list :(")
+			feedbackDiv.show();		
+		} else {
+			//if have rsvped -> show error
+			//if have rsvped but still have plus one -> allow to add plus one
+			//if has not rsvped-> continue to next Modal
+		}
 	});
-
-	// if(isValid){
-
-
-	// } else {
-	// 	//show alert?
-	// 	e.preventDefault();
-	// }
-
 });
+
+$('#rsvpModal').on('hidden.bs.modal', function(e){
+	$('.form-group').removeClass('has-error');
+	$('#rsvp-error-message-1').hide();
+	$('input').val('');
+})
