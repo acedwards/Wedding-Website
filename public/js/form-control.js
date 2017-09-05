ad2cdb5829bf9c8128166cf74a2b4d680dd56415
Andrea Stobo <andrea.stobo@gmail.com>
2017-09-05 00:09:37 -0400

    add multi-step modal

--- public/js/form-control.js
+++ public/js/form-control.js
@@ -1,44 +1,56 @@
-function validate(firstName, lastName) {
-	var fullName = firstName.val() + ' ' + lastName.val();
+function getGuest(firstName, lastName, Callback) {
+	var fullName = firstName + ' ' + lastName;
 	var database = firebase.database().ref('/guests/' + fullName).once('value').then(function(snapshot){
-		//if null return false else return true console.log(snapshot.val().isComing);
-		if(snapshot.val() === null){
-			console.log('snapshot is null');
-			return false;
-			
-		}else {
-			console.log('snapshot has stuff');
-			return snapshot.val();
-		}
+		guest = snapshot.val();
+		Callback(guest);
 	});
 }
 
-$('#next').on('click', function(e) {
+checkNames = function(e) {
 	firebase.auth().signInAnonymously().catch(function(error) {
 	// Handle Errors here.
 	var errorCode = error.code;
 	var errorMessage = error.message;
 	// ...
 	}).then(function() {
-		var firstName = $('#firstName');
-		var lastName = $('#lastName');
-		var isValid = validate(firstName, lastName);
-
-		if(isValid === false){
-			$('.form-group').addClass('has-error');
-			var feedbackDiv = $('#rsvp-error-message-1');
-			feedbackDiv.html("Oops! That name is not on the guest list :(")
-			feedbackDiv.show();		
-		} else {
-			//if have rsvped -> show error
-			//if have rsvped but still have plus one -> allow to add plus one
-			//if has not rsvped-> continue to next Modal
-		}
+		var firstName = $('#firstName').val();
+		var lastName = $('#lastName').val();
+		getGuest(firstName, lastName, function(guest){
+			if(guest === null){
+				$('.rsvp-form').addClass('has-error');
+				var feedbackDiv = $('#rsvp-error-message-1');
+				feedbackDiv.html("Oops! That name is not on the guest list :(")
+				feedbackDiv.show();		
+			} else {
+				//if have rsvped -> show warning -> would they like to make changes
+				if (guest.isComing !== -1){
+					$('.rsvp-form').addClass('has-warning');
+					var feedbackDiv = $('#rsvp-error-message-1');
+					feedbackDiv.html("You have already submitted your response!</br>If you'd like to make changes please contact Andrea at andrea.stobo@gmail.com")
+					feedbackDiv.show();	
+				}
+				//if has not rsvped-> continue to next Modal
+				else {
+					if (guest.plusOne === 1) {
+						$('#addGuest').show();
+					}
+					$('#firstNameHeader').html(firstName);
+					$('#lastNameHeader').html(lastName);
+					$('#rsvpModal').trigger('next.m.2');
+				}	
+			}
+		});
 	});
-});
+};
+
+updateDatabase = function(){
+
+}
 
 $('#rsvpModal').on('hidden.bs.modal', function(e){
 	$('.form-group').removeClass('has-error');
+	$('.form-group').removeClass('has-warning');
 	$('#rsvp-error-message-1').hide();
+	$('#rsvpModal').trigger('next.m.1');
 	$('input').val('');
 })
